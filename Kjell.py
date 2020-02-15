5d229d227d84398d657bbd26b7fada58e454ade3
Karl Minör <karl_minor@hotmail.com>
2020-02-15 14:18:44 +0100

    Quotes and new day command
    Only show the friday frog on fridays
    
    Added quotes from Kjell in four categories

--- Kjell.py
+++ Kjell.py
@@ -1,9 +1,14 @@
 from telegram.ext import Updater, CommandHandler
+from citat import fyra_sorter as f_s
+from citat import random_citat as r_c
+from citat import cigg_citat as c_c
+from citat import random_horse as r_h
 import secrets
 import requests
 import re
 import random
 import os
+import datetime
 
 def get_qoute():
     r = requests.get('http://inspirobot.me/api?generate=true')  
@@ -16,10 +21,41 @@ def inspiro(bot, update):
 
 def fredag(bot, update):
     chat_id = update.message.chat_id
-    directory = "pics/"
+    directory = "pics/fredag/"
     random_image = random.choice(os.listdir(directory))
     bot.send_message(chat_id=chat_id, text='Haha aa de ere')
-    bot.send_photo(chat_id=chat_id, photo=open('pics/'+random_image, 'rb'))
+    bot.send_photo(chat_id=chat_id, photo=open(directory+random_image, 'rb'))
+
+def dag(bot, update):
+    chat_id = update.message.chat_id
+    directory = "pics/intefredag/"
+    message = 'Nepp'
+    if (datetime.datetime.today().weekday() == 4):
+        message = 'Haha aa de ere'
+        directory = "pics/fredag/"
+    random_image = random.choice(os.listdir(directory))
+    bot.send_message(chat_id=chat_id, text=message)
+    bot.send_photo(chat_id=chat_id, photo=open(directory+random_image, 'rb'))
+    
+def fyra(bot, update):
+    chat_id = update.message.chat_id
+    message = f_s()
+    bot.send_message(chat_id=chat_id, text=message)
+    
+def citera(bot, update):
+    chat_id = update.message.chat_id
+    message = r_c()
+    bot.send_message(chat_id=chat_id, text=message)
+    
+def cigg(bot, update):
+    chat_id = update.message.chat_id
+    message = c_c()
+    bot.send_message(chat_id=chat_id, text=message)
+    
+def trav(bot, update):
+    chat_id = update.message.chat_id
+    message = r_h()
+    bot.send_message(chat_id=chat_id, text=message)
 
 def main():
     updater = Updater(secrets.api_token)
